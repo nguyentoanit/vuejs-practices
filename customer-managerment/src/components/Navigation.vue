b7a50605d2b514f11817aa29612d6edba1d3398c
Toan Nguyen <nguyentoandev@gmail.com>
2018-11-26 14:30:35 +0700

    Add navigation component

---
+++ customer-managerment/src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">\
+        <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li v-for="route in routes"><a v-bind:href="'#'+route.path">{{route.title}}</a></li>
+        </ul>
+    </div>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      routes: this.$router.options.routes
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- customer-managerment/src/router/index.js
+++ customer-managerment/src/router/index.js
@@ -1,21 +1,24 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Users from '@/components/user/Users'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Users from "@/components/user/Users";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  // mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
+      title: "Hello World",
       component: HelloWorld
     },
     {
-      path: '/users',
-      name: 'Users',
+      path: "/users",
+      name: "Users",
+      title: "Users",
       component: Users
-    },
+    }
   ]
-})
+});
