90cd53f00e537319b95165d4b97ca834d86a7e33
Toan Nguyen <nguyentoandev@gmail.com>
2018-11-30 17:39:48 +0700

    Store locale in Store

--- src/lang/lang.js
+++ src/lang/lang.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
+import store from '../store';
 import en from './en';
 import jp from './jp';
 import fr from './fr';
 Vue.use(VueI18n);
 
-const locale = 'jp';
-
+const locale = store.getters.getLocale;
+console.log(store);
 const messages = {
   en: en,
   jp: jp,
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import word from './modules/Word'
-Vue.use(Vuex)
+import Vue from 'vue';
+import Vuex from 'vuex';
+import word from './modules/Word';
+Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
+  state: {
+    locale: 'en'
+  },
+  getters: {
+    getLocale: state => {
+      return state.locale;
+    }
+  },
+  mutations: {
+    setLocale(state, locale) {
+      state.locale = locale;
+    }
+  },
   modules: {
     word: word
   }
-})
+});
